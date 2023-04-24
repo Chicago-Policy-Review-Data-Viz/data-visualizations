9d62ba7720095157a27a27fd1a66d63a2848cf08
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-04-23 23:19:01 -0500

    Update main.js

--- template-visual/main.js
+++ template-visual/main.js
@@ -7,7 +7,23 @@ am5.ready(function () {
 
     // Set themes
     // https://www.amcharts.com/docs/v5/concepts/themes/
-    root.setThemes([am5themes_Animated.new(root)]);
+    // Setting themes and default colors
+    var cprTheme = am5.Theme.new(root);
+    cprTheme
+        .rule('ColorSet')
+        .set('colors', [
+            am5.color('#800000'),
+            am5.color('#767676'),
+            am5.color('#FFA319'),
+            am5.color('#C16622'),
+            am5.color('#8F3931'),
+            am5.color('#8A9045'),
+            am5.color('#58593F'),
+            am5.color('#155F83'),
+            am5.color('#350E20')
+        ]);
+
+    root.setThemes([am5themes_Animated.new(root), cprTheme]);
 
     // All code for your chart goes here
 });
